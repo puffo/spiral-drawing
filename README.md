36b60b1c60b44474d9b2eedb20090ac37c1730ae
puffo <puffo@users.noreply.github.com>
2019-11-21 01:36:36 +0200

    :memo: Add instructions and a simple demo script

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # spiral-drawing
+
+An example solution for printing out a spiral shape via the console.
+
+Uses Ruby 2.3.8.
+
+## Getting up and running
+
+If you're feeling lazy, and would just like to see a bunch of different spirals:
+```
+ruby demo_spirals.rb
+```
+
+Otherwise, to add the spirals to your project, instantiate a spiral like so:
+```
+$ require './spiral.rb' #=> true
+$ Spiral.call(size: 5)
+```
+
+
+### Running tests
+
+Assuming your Ruby environment is setup correctly, simply add minitest:
+
+```
+gem install minitest
+```
+
+and run these commands:
+
+```bash
+ruby test/canvas_test.rb
+ruby test/spiral_test.rb
+```
---
+++ demo_spirals.rb
@@ -0,0 +1,33 @@
+require './spiral.rb'
+
+puts "------------------------------------------------------"
+puts "Here's a spiral of size 12:"
+puts ""
+Spiral.call(size: 12)
+
+puts ""
+puts "------------------------------------------------------"
+puts "Here's a spiral of size 10:"
+puts ""
+Spiral.call(size: 10)
+
+puts ""
+puts "------------------------------------------------------"
+puts "Here's a spiral of size 5:"
+puts ""
+Spiral.call(size: 5)
+
+puts ""
+puts "------------------------------------------------------"
+puts "Here's a spiral of size 3:"
+puts ""
+Spiral.call(size: 3)
+
+puts ""
+puts "------------------------------------------------------"
+puts "Here's a really big one of size 20 (for fun):"
+puts ""
+Spiral.call(size: 20)
+puts ""
+puts "------------------------------------------------------"
+puts ""
